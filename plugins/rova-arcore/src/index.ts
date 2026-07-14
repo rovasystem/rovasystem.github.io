13635654f4dd66a1d97d3b8757d7ef72e40c93ab
rovasystem <rova.system.solutions@gmail.com>
2026-07-14 12:04:55 +0200

    Delete plugins/rova-arcore directory

--- plugins/rova-arcore/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from "@capacitor/core";
-import type { RovaArCorePlugin } from "./definitions";
-
-const RovaArCore = registerPlugin<RovaArCorePlugin>("RovaArCore", {
-  web: () => import("./web").then((m) => new m.RovaArCoreWeb())
-});
-
-export * from "./definitions";
-export { RovaArCore };
